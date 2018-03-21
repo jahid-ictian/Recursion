bc01cd8a9385b89530840b87a90fdf3411b5f6d7
Md. Jahid Hasan <mjhprince1@gmail.com>
2018-03-21 17:07:03 +0600

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,32 @@
+//4*1+4*2+4*3+4*4.....up to n'th term
+#include<stdio.h>
+int N;
+int getsum(int i)
+{
+    if(i>N)
+    {
+        printf("\n");
+        return 0;
+    }
+    printf("%d",(3*i-1)*(2*i-1));
+    return (3*i-1)*(2*i-1)+getsum(i+1);
+}
+int main()
+{
+    int i,sum;
+    scanf("%d",&N);
+    printf("Iteration: \n");
+    sum=0;
+    for(i=1;i<=N;i++)
+    {
+       printf("%d",(3*i-1)*(2*i-1));
+       sum+=(3*i-1)*(2*i-1);
+    }
+    printf("\n");
+    printf("sum= %d\n",sum);
+    printf("\nRecursion: \n");
+    sum=getsum(1);
+    printf("Sum = %d\n",sum);
+    return 0;
+}
+
